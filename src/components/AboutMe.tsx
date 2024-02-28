d18c3b7ba4391c31b1f678fada344aef948ed6d8
Valentina Valverde <vvgreenville@gmail.com>
2024-02-28 12:48:12 -0500

    Created About Me component and added content

---
+++ src/components/AboutMe.tsx
@@ -0,0 +1,15 @@
+export default function AboutMe() {
+  return (
+    <>
+      <h1>About me</h1>
+      <p>
+        I enjoy UI/UX work as well as frontend development, though I am skilled
+        in fullstack applications. Small, fast-paced environments with
+        opportunity for growth catch my eye, and I have experience working on
+        small teams as well as directly with clients. I am open to work on
+        projects of any size or even test features on a new product! Don't be
+        shy!
+      </p>
+    </>
+  );
+}
